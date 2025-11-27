a18856ce512b052041635929c7db116be2d44215
akshayavinod204-del <akshaya.vinod204@gmail.com>
2025-11-27 15:55:01 +0530

    commit

--- student/studentapp/src/components/Signup.jsx
+++ student/studentapp/src/components/Signup.jsx
@@ -1,20 +1,51 @@
 import { Button, TextField } from '@mui/material'
-import React from 'react'
+import axios from 'axios'
+import React, {useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Signup = () => {
+const Signup = () =>{
+   var [val, setval] = useState({name:"",age:"",department:"",password:""})
+   var navigate=useNavigate() 
+   
+   
+   
+   const addfunction=(e)=>{
+    console.log(e.target.value);
+    setval({...val,[e.target.name]:e.target.value})
+        console.log(val);
+   }
+   
+const submitHandler=()=>{
+  axios.post("http://localhost:3000/addstudent",val)
+  .then((res)=>{
+    alert(res.data);
+     navigate("/table")
+  })
+}
+
   return (
     <div>
         <h2>Signup page</h2>
-        <TextField label="Name" variant="outlined" />
+        <TextField label="name" name='name' variant="outlined" 
+       Name='name' value={val.name} onChange={addfunction}
+        />  
         <br />
-        <TextField label="Age" variant="outlined" />          
+        <TextField label="Age" variant="outlined" 
+         name='age' value={val.age} onChange={addfunction}
+        
+        />          
         <br />
-        <TextField label="Department" variant="outlined" />
+        <TextField label="Department" variant="outlined" 
+        name='department' value={val.department} onChange={addfunction}
+        />
+
         <br />
-        <TextField label="Password" variant="outlined" />
+        <TextField label="Password" variant="outlined"
+        name='password' value={val.password} onChange={addfunction}
+        />
         <br />
 
-<Button variant="contained" color="success">
+<Button variant="contained"color="success" onClick={submitHandler}>
   Success
 </Button>        
     </div>
--- student/studentapp/src/components/TableView.jsx
+++ student/studentapp/src/components/TableView.jsx
@@ -1,46 +1,84 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
-
+import axios from "axios";
+import React, { useState } from "react";
+import {
+    Button,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@mui/material";
+import { Navigate } from "react-router-dom";
 
 const TableView = () => {
-    var [Student,setstudent]=useState([])
-          axios.get("http://localhost:3000/view")
-          .then((res)=>{
-            console.log(res.data)
-            setstudent(res.data)
-          })
-  return (
-    <div>
-        <TableContainer>
-      <Table >
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">id</TableCell>
-            <TableCell align="right">name</TableCell>
-            <TableCell align="right">age</TableCell>
-            <TableCell align="right">department</TableCell>
-            <TableCell align="right">password</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Student.map((val) => {
-            return(
-      
-             <TableRow>
-              <TableCell ></TableCell>
-              <TableCell ></TableCell>
-              <TableCell ></TableCell>
-              <TableCell ></TableCell>
-            </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
-  )
-}
+    var [Student, setstudent] = useState([]);
+    axios.get("http://localhost:3000/view").then((res) => {
+        console.log(res.data);
+        setstudent(res.data);
+    });
 
-export default TableView
+    const deletefunction = (id) => {
+        console.log(id);
+        axios.delete("http://localhost:3000/remove/" + id).then((res) => {
+            alert(res.data);
+        });
+
+    const updatefunction = (data) => {
+        Navigate("/s", { state: data });
+    }
+
+        return (
+            <div>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="right">id</TableCell>
+                                <TableCell align="right">name</TableCell>
+                                <TableCell align="right">age</TableCell>
+                                <TableCell align="right">department</TableCell>
+                                <TableCell align="right">password</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {Student.map((val) => {
+                                return (
+                                    <TableRow>
+                                        <TableCell>{val.name}</TableCell>
+                                        <TableCell>{val.age}</TableCell>
+                                        <TableCell>{val.department}</TableCell>
+                                        <TableCell>{val.password}</TableCell>
+                                        <Button
+                                            variant="contained"
+                                            color="error"
+                                            onClick={() => {
+                                                deletefunction(val._id);
+                                            }}
+                                        >
+                                            delete
+                                        </Button>
+
+
+
+
+                                         <Button
+                                            variant="contained"
+                                            color="error"
+                                            onClick={() => {
+                                                updatefunction(val._id);
+                                            }}
+                                        >
+                                            update
+                                        </Button>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        );
+    };
+};
+export default TableView;
